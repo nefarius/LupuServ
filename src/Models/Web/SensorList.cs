14f36216c451ae33de470f3e0db80fad0b54d1c1
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-02-27 13:05:11 +0100

    Adding supportive types

---
+++ src/Models/Web/SensorList.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LupuServ.Models.Web;
+
+public sealed class SensorListResponse
+{
+    [JsonPropertyName("senrows")]
+    public List<Senrow> Senrows { get; set; }
+}
+
+public sealed class Senrow
+{
+    [JsonPropertyName("no")]
+    public string No { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("zone")]
+    public string Zone { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("attr")]
+    public string Attr { get; set; }
+
+    [JsonPropertyName("cond")]
+    public string Cond { get; set; }
+
+    [JsonPropertyName("battery")]
+    public string Battery { get; set; }
+
+    [JsonPropertyName("tamp")]
+    public string Tamp { get; set; }
+
+    [JsonPropertyName("bypass")]
+    public string Bypass { get; set; }
+}
---
+++ src/Util/BasicAuthHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace LupuServ.Util;
+
+internal class BasicAuthHandler : HttpClientHandler
+{
+    private readonly string _password;
+    private readonly string _username;
+
+    public BasicAuthHandler(string username,
+        string password)
+    {
+        _username = username;
+        _password = password;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        request.Headers.Authorization = new AuthenticationHeaderValue("Basic",
+            Convert.ToBase64String(Encoding.ASCII.GetBytes(
+                $"{_username}:{_password}")));
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
